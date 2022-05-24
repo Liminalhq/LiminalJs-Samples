6dcb8d270add49f158a854d7b4362246fb82b828
Kishor Naik <kishor@lmnl.app>
2022-05-24 10:39:02 +0530

    Code Commit : Consoldidate Transaction

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,3 +1,5 @@
+import { tsmCreds } from "@lmnl/liminaljs";
+
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -7,3 +9,10 @@ export const clientId=process?.env?.CLIENT_ID;
 export const clientSecretId=process?.env?.CLIENT_SECRET_ID;
 
 export const walletId=process?.env?.WALLET_ID;
+
+export const tsmCred:tsmCreds={
+    url:process?.env?.TSM_URL,
+    userID:process?.env?.TSM_USER_ID,
+    password:process?.env?.TSM_PASSWORD,
+    publicKey:process?.env?.TSM_PUBLIC_KEY
+}
