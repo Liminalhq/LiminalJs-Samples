aef27b85a22d4c20972675f6f03a0da7a84b9872
Kishor Naik <kishor@lmnl.app>
2022-07-11 13:09:56 +0530

    Code Commit: Remove MPC Infra

--- src/Cli/mpc/infrastructure/tar-original.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu
-
-/usr/bin/sudo apt-get install zip -y
-
-/usr/bin/zip --password passwd infrastructure-config.zip -r config
-
-######## end ########
--- src/Cli/mpc/ssh/readme.txt
+++
@@ -1,3 +0,0 @@
-######## ssh key to log into the created EC2 instance ########
-
-######## Warning: DO not delete this file, it may cause the script to malfunction ########
