3ca11625a03ba1b9bd1ecba1618849db5d06b522
Kishor Naik <kishor@lmnl.app>
2022-11-22 11:42:17 +0530

    Code Commit: Single Transaction using Deposit Wallet

--- src/Examples/SDK-Features-Samples/Deposit/TransferFunds/EVM/Index.ts
+++ src/Examples/SDK-Features-Samples/Deposit/TransferFunds/EVM/Index.ts
@@ -3,10 +3,13 @@ import { LiminalAuthAsync, SendManyTransactionAsync, WalletInstanceAsync } from 
 import { Guid } from "guid-typescript";
 import { clientId, clientSecretId, env, tsmCred } from "../../../../../Settings";
 
+
 /**
- * Run Command => npm run start:std-mpc
+ * Single Transaction using deposit Wallet
+ * Run Command => npm run start:stdevm
  * Docs => https://docs.lmnl.app/docs/sendmany
  */
+
 export const main=async():Promise<void>=>{
 
     try
@@ -23,11 +26,11 @@ export const main=async():Promise<void>=>{
         // Step 2: Get Wallet Instance
         let walletInstance:Wallet=await WalletInstanceAsync({
             liminalJs:liminalJs,
-            coin:CoinsEnum.sol, // Define your Coin here
-            walletId:3466 // Define your Deposit wallet Id
+            coin:CoinsEnum.eth, // Define Your Coin here
+            walletId:1609 // Define Your Coin Wallet Id Here
         });
 
-        // Step 3 : Send Transaction
+        // Step 3: Data Send Transaction 
         let sequenceId:string=Guid.create().toString();
         console.log("sequenceId =>", sequenceId);
 
@@ -36,24 +39,28 @@ export const main=async():Promise<void>=>{
             recipientsData:{
                 recipients:[
                     {
-                        address:"3xZrNy2CdZ5yVoX9eSinrVuxWVH8t8ezp3eyCRGsrUNM",
-                        amount:0.01,
-                        //data:""  //=> If you are using XRP
+                        address:"0x20572e4c090f15667cF7378e16FaD2eA0e2f3EfF",
+                        amount:0.00001,
+                        data:"0x"
                     },
 
                 ],
+                fromAddress:"0xC92745038c520446d1fb88c84Da268A22cfFDEB8",
+                gasLimit:240000,
                 sequenceId:sequenceId,
                 tsmCreds:tsmCred
             }
         });
 
         if(transactionResponse?.success===true){
+
             console.log("Transaction Response =>",JSON.stringify(transactionResponse?.data));
         }
         else
         {
             console.log(`Error => Transaction Response => ${transactionResponse?.message}`);
-        }        
+        }
+
     }
     catch(ex)
     {
